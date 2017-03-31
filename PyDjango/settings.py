8e020206112599c281ea599d8742195e0a097b1c
ChunHoYeung <chunhoyeung13@gmail.com>
2017-03-31 11:11:40 +1100

    Add templates directory in the settings.py

--- PyDjango/settings.py
+++ PyDjango/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'PyDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['PyDjango/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
